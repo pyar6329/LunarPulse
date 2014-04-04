8256fe9a4fa576f703faeeee07c194fb4588058f
pyar6329 <pyar6329@gmail.com>
2014-04-04 17:13:39 +0000

    $ rails g controller TestPage index #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LunarPulse::Application.routes.draw do
+  get "test_page/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
