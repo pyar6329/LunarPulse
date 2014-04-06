c554d6210014d96249be2ead8f22edb16a63520e
pyar6329 <pyar6329@gmail.com>
2014-04-07 08:28:38 +0900

    route sets moon stereo #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 LunarPulse::Application.routes.draw do
-  get "moon_stereo/index"
+
   root 'test_page#index'    #トップページ
 
   get 'map/' => 'moon_map#index' # 月面マップ画面
+  get 'stereo/' => 'moon_stereo#index' #月面ステレオ画面
 
+  # get "moon_stereo/index"
   # get "moon_map/index"
   # get "test_page/index"
   # The priority is based upon order of creation: first created -> highest priority.
