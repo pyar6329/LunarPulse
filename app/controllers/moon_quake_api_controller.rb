e87470de785a8518c8d8ef55fa2295316efa7640
ryohei-suda <m5161129@u-aizu.ac.jp>
2014-04-12 19:14:09 +0000

    Modified api #4

--- app/controllers/moon_quake_api_controller.rb
+++ app/controllers/moon_quake_api_controller.rb
@@ -5,24 +5,30 @@ class MoonQuakeApiController < ApplicationController
   end
 
   def getDuration
-    @amp = Amplitude.where('time >= ? AND time <= ?', params[:start], params[:end])
+    @amp = Amplitude.joins(:moon_quake).where('time >= ? AND time <= ?', params[:start], params[:end]).select('amplitudes.id', 'amplitude', 'time', 'moon_quake_id', 'quake_category_id', 'seismometer_id', 'lat', 'lng')
     render :json => @amp.to_json
   end
 
   def getSiteDuration
-    @amp = Amplitude.joins(:moon_quake).where('seismometer_id = ? AND time >= ? AND time <= ?', params[:site], params[:start], params[:end])
+    @amp = Amplitude.joins(:moon_quake).where('seismometer_id = ? AND time >= ? AND time <= ?', params[:site], params[:start], params[:end]).select('amplitudes.id', 'amplitude', 'time', 'moon_quake_id', 'quake_category_id', 'seismometer_id', 'lat', 'lng')
     render :json => @amp.to_json
   end
 
   def getStartDate
     time = Amplitude.maximum(:time)
-    render :json => {'start' => time}
+    render :json => {'year' => time.year, 'month' => time.month, 'day' => time.day}
   end
 
   def getEndDate
     time = Amplitude.minimum(:time)
-    render :json => {'end' => time}
+    render :json => {'year' => time.year, 'month' => time.month, 'day' => time.day}
   end
 
-end
+  def getAllSeismometer
+    render :json => Seismometer.all.select('id', 'name', 'color').to_json
+  end
 
+  def getAllQuakeCategory
+    render :json => QuakeCategory.all.select('id', 'name', 'color').to_json
+  end
+end
