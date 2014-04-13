52d2d2cd08eda884aa17ffdda6136b2fd695c4a1
ryohei-suda <m5161129@u-aizu.ac.jp>
2014-04-13 01:02:58 +0000

    Added hueConrol parts

--- app/assets/javascripts/moon_stereo/getMoonQuake.js
+++ app/assets/javascripts/moon_stereo/getMoonQuake.js
@@ -9,9 +9,11 @@ $(function(){
     console.log(URL);
     var Data = $.get(URL, function(data){
         for(i=0; i<data.length - 1; i++){
-            var listenTime = data[i+1].time - data[i]; // 取得した月震の間処理する
-            console.log(data[i].time);
+            var listenTime = data[i+1].time - data[i].time; // 取得した月震の間処理する
 
+            var timeParts = data[i].time.split('T');
+            timeParts = timeParts[0].split('-');
+            setMoonAge(calcMoonAge(timeParts[0], timeParts[1], timeParts[2]));
         }
 //        console.log(data[0].id);
     });
