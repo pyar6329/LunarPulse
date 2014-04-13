9110e9f178a30cdc2013703423e2481b2ddb735c
pyar6329 <pyar6329@gmail.com>
2014-04-13 11:44:34 +0900

    fix confrict date_from and date_to #2

--- app/controllers/moon_map_controller.rb
+++ app/controllers/moon_map_controller.rb
@@ -1,8 +1,6 @@
 class MoonMapController < ApplicationController
   def index
-    unless params[:day_from].nil?
-      @start = Date.new(params[:year_from].to_i, params[:month_from].to_i, params[:day_from].to_i).to_s
-      @end = Date.new(params[:year_to].to_i, params[:month_to].to_i, params[:day_to].to_i).to_s
-    end
+    @start = params[:date_from]
+    @end = params[:date_to]
   end
 end
