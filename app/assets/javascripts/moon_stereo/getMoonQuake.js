687bec3abf042d2ebaab78807420918fdfbcc180
pyar6329 <pyar6329@gmail.com>
2014-04-13 09:10:58 +0900

    include audio and hue javascript files #3

--- app/assets/javascripts/moon_stereo/getMoonQuake.js
+++ app/assets/javascripts/moon_stereo/getMoonQuake.js
@@ -1,6 +1,6 @@
 var hostURL = "http://lunarpulse.herokuapp.com";
 var startDate = "1976-01-01";
-var endDate = "/1976-02-02";
+var endDate = "1976-02-02";
 
 $(function(){
 //    var seisId = $(this).attr('id');
@@ -8,8 +8,10 @@ $(function(){
     var URL = "/moon_quake_api/duration/" + startDate + "/" + endDate + "/site/"+ seisId;
     console.log(URL);
     var Data = $.get(URL, function(data){
-        for(i=0; i<data.length; i++){
-            console.log(data[i].moon_quake_id);
+        for(i=0; i<data.length - 1; i++){
+            var listenTime = data[i+1].time - data[i]; // 取得した月震の間処理する
+            console.log(data[i].time);
+
         }
 //        console.log(data[0].id);
     });
